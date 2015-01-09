3b0274ea1988d5b673d06e5be57bd72759666e10
Adam Pflantzer <apflantzer@gmail.com>
2015-01-09 10:29:14 -0500

    Wordpress Marker

--- js/controllers.js
+++ js/controllers.js
@@ -34,7 +34,7 @@ angular.module('subcontractorsApp.controllers', [])
                         var frontCutDescrip = descriptionContract.slice(3);
                         var finalDescription = frontCutDescrip.slice(0,-5);
                         var abbrState = convert_state(contract.custom_fields.vendor_state[0],'abbrev');
-                        var region = convert_state(contract.custom_fields.vendor_state[0],'region');
+                        var region = convert_state(contract.custom_fields.principal_performance_state[0],'region');
                         var naics = contract.custom_fields.naics_code[0];
                         var naicsFront = naics.substr(0, 3);
                         var naicsBack = naics.substr(naics.length - 3);
@@ -63,6 +63,7 @@ angular.module('subcontractorsApp.controllers', [])
                     });
                 }
                 $scope.makeContractors();
+
 
             }); // close async
         }; // close getData function
@@ -111,23 +112,25 @@ angular.module('subcontractorsApp.controllers', [])
         // CONVERT STATES
         function convert_state(name, to) {
             var name = name.toUpperCase();
-            var states = new Array(                         {'name':'Alabama', 'abbrev':'AL', 'region':'4'},          {'name':'Alaska', 'abbrev':'AK', 'region':'10'},
+            var states = new Array(
+                {'name':'District of Columbia', 'abbrev':'DC', 'region':'11'},          {'name':'Alabama', 'abbrev':'AL', 'region':'4'},          {'name':'Alaska', 'abbrev':'AK', 'region':'10'},
                 {'name':'Arizona', 'abbrev':'AZ', 'region':'9'},          {'name':'Arkansas', 'abbrev':'AR', 'region':'7'},         {'name':'California', 'abbrev':'CA', 'region':'9'},
-                {'name':'Colorado', 'abbrev':'CO', 'region':'8'},         {'name':'Connecticut', 'abbrev':'CT', 'region':'1'},      {'name':'Delaware', 'abbrev':'DE', 'region':'3'},
+                {'name':'Colorado', 'abbrev':'CO', 'region':'8'},         {'name':'Connecticut', 'abbrev':'CT', 'region':'01'},      {'name':'Delaware', 'abbrev':'DE', 'region':'3'},
                 {'name':'Florida', 'abbrev':'FL', 'region':'4'},          {'name':'Georgia', 'abbrev':'GA', 'region':'4'},          {'name':'Hawaii', 'abbrev':'HI', 'region':'9'},
-                {'name':'Idaho', 'abbrev':'ID', 'region':'10'},            {'name':'Illinois', 'abbrev':'IL', 'region':'5'},         {'name':'Indiana', 'abbrev':'IN', 'region':'5'},
+                {'name':'Idaho', 'abbrev':'ID', 'region':'10'},           {'name':'Illinois', 'abbrev':'IL', 'region':'5'},         {'name':'Indiana', 'abbrev':'IN', 'region':'5'},
                 {'name':'Iowa', 'abbrev':'IA', 'region':'6'},             {'name':'Kansas', 'abbrev':'KS', 'region':'6'},           {'name':'Kentucky', 'abbrev':'KY', 'region':'4'},
-                {'name':'Louisiana', 'abbrev':'LA', 'region':'7'},        {'name':'Maine', 'abbrev':'ME', 'region':'1'},            {'name':'Maryland', 'abbrev':'MD', 'region':'3'},
-                {'name':'Massachusetts', 'abbrev':'MA', 'region':'1'},    {'name':'Michigan', 'abbrev':'MI', 'region':'5'},         {'name':'Minnesota', 'abbrev':'MN', 'region':'5'},
+                {'name':'Louisiana', 'abbrev':'LA', 'region':'7'},        {'name':'Maine', 'abbrev':'ME', 'region':'01'},            {'name':'Maryland', 'abbrev':'MD', 'region':'3'},
+                {'name':'Massachusetts', 'abbrev':'MA', 'region':'01'},    {'name':'Michigan', 'abbrev':'MI', 'region':'5'},         {'name':'Minnesota', 'abbrev':'MN', 'region':'5'},
                 {'name':'Mississippi', 'abbrev':'MS', 'region':'4'},      {'name':'Missouri', 'abbrev':'MO', 'region':'6'},         {'name':'Montana', 'abbrev':'MT', 'region':'8'},
-                {'name':'Nebraska', 'abbrev':'NE', 'region':'6'},         {'name':'Nevada', 'abbrev':'NV', 'region':'1'},           {'name':'New Hampshire', 'abbrev':'NH', 'region':'1'},
+                {'name':'Nebraska', 'abbrev':'NE', 'region':'6'},         {'name':'Nevada', 'abbrev':'NV', 'region':'9'},           {'name':'New Hampshire', 'abbrev':'NH', 'region':'01'},
                 {'name':'New Jersey', 'abbrev':'NJ', 'region':'2'},       {'name':'New Mexico', 'abbrev':'NM', 'region':'7'},       {'name':'New York', 'abbrev':'NY', 'region':'2'},
                 {'name':'North Carolina', 'abbrev':'NC', 'region':'4'},   {'name':'North Dakota', 'abbrev':'ND', 'region':'8'},     {'name':'Ohio', 'abbrev':'OH', 'region':'5'},
                 {'name':'Oklahoma', 'abbrev':'OK', 'region':'7'},         {'name':'Oregon', 'abbrev':'OR', 'region':'10'},           {'name':'Pennsylvania', 'abbrev':'PA', 'region':'3'},
-                {'name':'Rhode Island', 'abbrev':'RI', 'region':'1'},     {'name':'South Carolina', 'abbrev':'SC', 'region':'4'},   {'name':'South Dakota', 'abbrev':'SD', 'region':'8'},
+                {'name':'Rhode Island', 'abbrev':'RI', 'region':'01'},     {'name':'South Carolina', 'abbrev':'SC', 'region':'4'},   {'name':'South Dakota', 'abbrev':'SD', 'region':'8'},
                 {'name':'Tennessee', 'abbrev':'TN', 'region':'4'},        {'name':'Texas', 'abbrev':'TX', 'region':'7'},            {'name':'Utah', 'abbrev':'UT', 'region':'8'},
-                {'name':'Vermont', 'abbrev':'VT', 'region':'1'},          {'name':'Virginia', 'abbrev':'VA', 'region':'3'},         {'name':'Washington', 'abbrev':'WA', 'region':'10'},
-                {'name':'West Virginia', 'abbrev':'WV', 'region':'3'},    {'name':'Wisconsin', 'abbrev':'WI', 'region':'5'},        {'name':'Wyoming', 'abbrev':'WY', 'region':'8'}
+                {'name':'Vermont', 'abbrev':'VT', 'region':'01'},          {'name':'Virginia', 'abbrev':'VA', 'region':'3'},         {'name':'Washington', 'abbrev':'WA', 'region':'10'},
+                {'name':'West Virginia', 'abbrev':'WV', 'region':'3'},    {'name':'Wisconsin', 'abbrev':'WI', 'region':'5'},        {'name':'Wyoming', 'abbrev':'WY', 'region':'8'},
+                {'name':'Puerto Rico', 'abbrev':'PR', 'region':'2'},      {'name':'Virgin Islands', 'abbrev':'VI', 'region':'2'}
             );
             var returnthis = false;
             $.each(states, function(index, value){
@@ -142,7 +145,7 @@ angular.module('subcontractorsApp.controllers', [])
                         return false;
                     }
                 } else if (to == 'region') {
-                    if (value.name.toUpperCase() == name){
+                    if (value.abbrev.toUpperCase() == name){
                         returnthis = value.region;
                         return false;
                     }
